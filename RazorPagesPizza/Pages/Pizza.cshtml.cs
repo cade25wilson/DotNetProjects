16de48b46c1940afd27cba52d307e0de0af824ff
Cade Wilson <cade25wilson@gmail.com>
2021-11-02 15:26:00 -0600

    Initial commit

---
+++ RazorPagesPizza/Pages/Pizza.cshtml.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesPizza.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        public void OnGet()
+        {
+            pizzas = PizzaService.GetAll();
+            public List<Pizza> pizzas;
+        }
+
+        public string GlutenFreeText(Pizza pizza)
+        {
+            if (pizza.IsGlutenFree)
+                return "Gluten Free";
+            return "Not Gluten Free";
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            PizzaService.Add(NewPizza);
+            return RedirectToAction("Get");
+        }
+
+        [BindProperty]
+        public Pizza NewPizza { get; set; }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            PizzaService.Delete(id);
+            return RedirectToAction("Get");
+        }
+    }
+}
