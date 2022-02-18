fc37a475e80db0dc708ccd0c71e378b436f162fa
Cade Wilson <cade25wilson@gmail.com>
2022-02-18 11:18:51 -0700

    categories, Statuses
    created categories and statuses

--- SuggestionAppUI/SuggestionAppLibrary/Models/BasicUserModel.cs
+++ SuggestionAppUI/SuggestionAppLibrary/Models/BasicUserModel.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SuggestionAppLibrary.Models;
 
-namespace SuggestionAppLibrary.Models;
-
-internal class BasicUserModel
+public class BasicUserModel
 {
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string DisplayName { get; set; }
+
+    public BasicUserModel()
+    {
+
+    }
+
+    public BasicUserModel(UserModel user)
+    {
+        Id = user.Id;
+        DisplayName = user.DisplayName; 
+    }
 }
--- SuggestionAppUI/SuggestionAppLibrary/Models/UserModel.cs
+++ SuggestionAppUI/SuggestionAppLibrary/Models/UserModel.cs
@@ -7,7 +7,7 @@ public class UserModel
     public string Id { get; set; }
     public string ObjectIdentifier { get; set; }
     public string Firstname { get; set; }
-    public string  LastName { get; set; }
+    public string LastName { get; set; }
     public string DisplayName { get; set; }
     public string EmailAddress { get; set; }
     public List<BasicSuggestionModel> AuthoredSuggestions { get; set; } = new();
